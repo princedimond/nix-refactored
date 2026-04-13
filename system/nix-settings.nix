5402210b7566f0754f7ebf75df6be8397543c8c9
princedimond <princedimond@gmail.com>
2026-04-13 14:30:51 -0500

    added cache
    added cache for nix binary packages to prevent building from source as
    much as possible need the substituter and the trusted public key for
    each cache

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -5,6 +5,12 @@
       "nix-command"
       "flakes"
     ];
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;

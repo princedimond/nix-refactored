0deb1324471857dddd0d2ebd2e70b79da8808fd9
princedimond <princedimond@gmail.com>
2026-04-16 15:43:22 -0500

    fix git settings

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -2,7 +2,9 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    user.name = "princedimond";
-    user.email = "princedimond@gmail.com";
+    settings = {
+      user.name = "princedimond";
+      user.email = "princedimond@gmail.com";
+    };
   };
 }

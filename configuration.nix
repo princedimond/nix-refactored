09cc75ee5691204ad45c6e3bd8a49585f08d6324
princedimond <princedimond@gmail.com>
2026-04-16 15:12:04 -0500

    add home manager module to nixos
    added home manager module so its able to be used by all hosts

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,22 @@
+{ username, inputs, ... }:
 {
   imports = [
     ./system
     ./modules
     ./options.nix
   ];
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit username;
+      inherit inputs;
+    };
+    backupFileExtension = "hmBAK";
+    users.${username} = {
+      imports = [
+        ./home
+      ];
+    };
+  };
 
 }

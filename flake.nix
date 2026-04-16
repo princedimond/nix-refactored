9e4e11faab1c1c8badcca3c62357d4dae7262704
princedimond <princedimond@gmail.com>
2026-04-16 15:23:25 -0500

    added additional nix packages repos
    change base to unstable, add options for stable and master packages
    where required. be sure to remember to imort stable & master in the
    files where you need to reference them for the package you specifically
    want to change

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "pd-nix-refactored";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs-master.url = "github:nixos/nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,6 +15,8 @@
     {
       self,
       nixpkgs,
+      nixpkgs-stable,
+      nixpkgs-master,
       ...
     }@inputs:
     let
@@ -27,6 +31,14 @@
           {
             inherit username;
             inherit inputs;
+            stable = import nixpkgs-stable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            master = import nixpkgs-master {
+              inherit system;
+              config.allowUnfree = true;
+            };
           };
         modules = [
           ./hosts/template
